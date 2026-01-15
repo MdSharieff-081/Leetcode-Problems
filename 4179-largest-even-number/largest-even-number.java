595d846f321e886e4e5ab348aacf12e03242efbc
Mohammad Chisty Madeena Sharieff <115969107+MdSharieff-081@users.noreply.github.com>
2026-01-15 20:36:50 +0530

    Time: 1 ms (58.80%) | Memory: 44.4 MB (5.71%) - LeetSync

--- 4179-largest-even-number/largest-even-number.java
+++ 4179-largest-even-number/largest-even-number.java
@@ -3,14 +3,14 @@ class Solution {
         StringBuilder sb=new StringBuilder(s);
 
         
-        if(sb.length()==1){
-            if(sb.charAt(0)=='1'){
-                return "";
-           }
-           else{
-            return sb.toString();
-           }
-        }
+        // if(sb.length()==1){
+        //     if(sb.charAt(0)=='1'){
+        //         return "";
+        //    }
+        //    else{
+        //     return sb.toString();
+        //    }
+        // }
         int n=sb.length()-1;
         for(int i=n;i>=0;i--){
             if(sb.charAt(i)=='2'){
