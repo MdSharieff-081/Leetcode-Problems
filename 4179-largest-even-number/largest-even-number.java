98de9e3cdf234ed8c207149a62dc3e4fefb95a89
Mohammad Chisty Madeena Sharieff <115969107+MdSharieff-081@users.noreply.github.com>
2026-01-15 20:36:36 +0530

    Time: 1 ms (58.80%) | Memory: 44.3 MB (30.38%) - LeetSync

---
+++ 4179-largest-even-number/largest-even-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String largestEven(String s) {
+        StringBuilder sb=new StringBuilder(s);
+
+        
+        if(sb.length()==1){
+            if(sb.charAt(0)=='1'){
+                return "";
+           }
+           else{
+            return sb.toString();
+           }
+        }
+        int n=sb.length()-1;
+        for(int i=n;i>=0;i--){
+            if(sb.charAt(i)=='2'){
+                return sb.substring(0,i+1);
+            }
+            // else{
+            //     //return sb.toString();
+            // }
+        }
+        return "";
+    }
+}
